9f1c7b9caf7b105ddee502bd205f3715f0749bb6
hoangvu9999 <hoanghoangvu99@gmail.com>
2021-04-06 15:24:11 +0700

    update

---
+++ main.js
@@ -0,0 +1,24 @@
+let mainNav = document.querySelector(".main-nav-ul > li > a");
+let subMenu = document.querySelector(".main-nav-ul li > ul");
+let subNav = document.querySelectorAll(".sub-nav-ul > li > a");
+
+let checkMain = true;
+mainNav.addEventListener("click", function () {
+    subMenu.style.display = checkMain ? "block" : "none";
+    checkMain = !checkMain;
+});
+
+let checkSub = true;
+for (let i = 0; i < subNav.length; i++) {
+    checkSub = true
+    subNav[i].addEventListener("click", function (e) {
+        let listLi = document.querySelectorAll(".level2");
+        listLi.forEach(function (li) {
+            li.style.display = "none";
+        })
+
+        let li = e.srcElement.parentElement.children;
+        li[1].style.display = checkSub ? "block" : "none";
+        checkSub = !checkSub
+    });
+}
