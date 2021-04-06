bbefd08e02c9430634ea73f7051bd6096f090b9d
hoangvu9999 <hoanghoangvu99@gmail.com>
2021-04-06 22:49:16 +0700

    update

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-let mainNav = document.querySelector(".main-nav-ul > li > a");
+let mainNav = document.querySelectorAll(".main-nav-ul > li > a");
 let subMenu = document.querySelector(".main-nav-ul li > ul");
 let subNav = document.querySelectorAll(".sub-nav-ul > li > a");
  
-let checkMain = true;
-mainNav.addEventListener("click", function () {
-    subMenu.style.display = checkMain ? "block" : "none";
-    checkMain = !checkMain;
-});
 
-let checkSub = true;
+for (let i = 0; i < mainNav.length; i++) {
+    mainNav[i].addEventListener("click", function (e) {
+        let li = e.srcElement.parentElement.children;
+        li[1].style.display = !li[1].style.display || li[1].style.display == "none" ? "block" : "none";
+    });
+}
 for (let i = 0; i < subNav.length; i++) {
-    checkSub = true
     subNav[i].addEventListener("click", function (e) {
         let listLi = document.querySelectorAll(".level2");
         listLi.forEach(function (li) {
             li.style.display = "none";
         })
         let li = e.srcElement.parentElement.children;
-        li[1].style.display = checkSub ? "block" : "none";
-        checkSub = !checkSub
+        li[1].style.display = !li[1].style.display || li[1].style.display == "none" ? "block" : "none";
     });
 }
